c285d365affe4222e854882aa82ed0e8cb3993bc
ibnu naufal <ibnunaufal5758@gmail.com>
2023-01-18 09:27:55 +0700

    Initial Commit

---
+++ FruitApp/FruitAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitAppApp.swift
+//  FruitApp
+//
+//  Created by Macbook Pro on 18/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct FruitAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
