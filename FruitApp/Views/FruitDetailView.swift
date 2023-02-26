5eff6c1b9065ea9f857a4fd1ff826a94533987c5
ibnu naufal <ibnunaufal5758@gmail.com>
2023-02-26 11:35:31 +0700

    fruitdetail part 1

---
+++ FruitApp/Views/FruitDetailView.swift
@@ -0,0 +1,62 @@
+//
+//  FruitDetailView.swift
+//  FruitApp
+//
+//  Created by Macbook Pro on 26/02/2023.
+//
+
+import SwiftUI
+
+struct FruitDetailView: View {
+    
+    var fruit: Fruit
+    
+    var body: some View {
+        NavigationView(){
+            ScrollView(.vertical, showsIndicators: false){
+                VStack(alignment: .center, spacing: 20){
+                    // Header
+                    FruitHeaderView(fruit: fruit)
+                    VStack(alignment: .leading, spacing: 20) {
+                        // title
+                        Text(fruit.title)
+                            .font(.largeTitle)
+                            .fontWeight(.heavy)
+                            .foregroundColor(fruit.gradientColors[1])
+                        
+                        // headline
+                        Text(fruit.headline)
+                            .font(.headline)
+                            .multilineTextAlignment(.leading)
+                        
+                        // nutririon
+                        
+                        // subheadline
+                        Text("Learn more about \(fruit.title)".uppercased())
+                            .fontWeight(.bold)
+                            .foregroundColor(fruit.gradientColors[1])
+                        
+                        // desc
+                        Text(fruit.description)
+                            .multilineTextAlignment(.leading)
+                        // link
+                        SourceLinkView()
+                            .padding(.top, 10)
+                            .padding(.bottom, 40)
+                    }// VSTACK
+                    .padding(.horizontal, 20)
+                    .frame(maxWidth: 640, alignment: .center)
+                }// VSTACK
+                .navigationBarTitle(fruit.title, displayMode: .inline)
+                .navigationBarHidden(true)
+            }// Scroll
+            .edgesIgnoringSafeArea(.top)
+        }// Navigation
+    }
+}
+
+struct FruitDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        FruitDetailView(fruit: fruitsData[0])
+    }
+}
